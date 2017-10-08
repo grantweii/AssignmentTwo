17e3a8e1938d882d12a9d0a72a1c128608fd4317
Joshua Glover <Glover@joshuas-mbp.lan>
2017-10-08 23:04:30 +1100

    Terrain Changes

--- src/ass2/spec/Game.java
+++ src/ass2/spec/Game.java
@@ -1,5 +1,7 @@
 package ass2.spec;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import com.jogamp.opengl.*;
@@ -7,113 +9,146 @@ import com.jogamp.opengl.awt.GLJPanel;
 import javax.swing.JFrame;
 import com.jogamp.opengl.util.FPSAnimator;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
 
 /**
- * COMMENT: Comment Game 
+ * COMMENT: Comment Game
  *
  * @author malcolmr
  */
-public class Game extends JFrame implements GLEventListener{
+public class Game extends JFrame implements GLEventListener, KeyListener{
 
     private Terrain myTerrain;
+    private double num;
 
     public Game(Terrain terrain) {
-    	super("Assignment 2");
+        super("Assignment 2");
         myTerrain = terrain;
-   
+
     }
-    
-    /** 
+
+    /**
      * Run the game.
      *
      */
     public void run() {
-    	  GLProfile glp = GLProfile.getDefault();
-          GLCapabilities caps = new GLCapabilities(glp);
-          GLJPanel panel = new GLJPanel();
-          panel.addGLEventListener(this);
- 
-          // Add an animator to call 'display' at 60fps        
-          FPSAnimator animator = new FPSAnimator(60);
-          animator.add(panel);
-          animator.start();
+        GLProfile glp = GLProfile.getDefault();
+        GLCapabilities caps = new GLCapabilities(glp);
+        GLJPanel panel = new GLJPanel();
+        panel.addGLEventListener(this);
 
-          getContentPane().add(panel);
-          setSize(800, 600);
-          setVisible(true);
-          setDefaultCloseOperation(EXIT_ON_CLOSE);        
+        // Add an animator to call 'display' at 60fps
+        FPSAnimator animator = new FPSAnimator(60);
+        animator.add(panel);
+        animator.start();
+
+        panel.addKeyListener(this);
+
+        getContentPane().add(panel);
+        setSize(800, 600);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
+
     /**
      * Load a level file and display it.
-     * 
+     *
      * @param args - The first argument is a level file in JSON format
      * @throws FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
         Terrain terrain = LevelIO.load(new File(args[0]));
-        int x, z;
-        for (z = 0; z < 5; z++) {
-            for (x = 0; x < 5; x++) {
-                System.out.print(terrain.getGridAltitude(x,z) + " ");
-            }
-            System.out.println();
-        }
-
         Game game = new Game(terrain);
         game.run();
+
     }
 
-	@Override
-	public void display(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        // TODO Auto-generated method stub
         GL2 gl = drawable.getGL().getGL2();
-
         gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity();
-
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
-        // Back face culling on
-        int z, x;
-        for (z = 0; z < myTerrain.size().width-1; z++) {
-            for (x = 0; x < myTerrain.size().height-1; x++) {
-                gl.glPolygonMode(GL2.GL_FRONT_AND_BACK,GL2.GL_LINE);
-                gl.glBegin(GL2.GL_TRIANGLE_FAN);{
-                    gl.glColor3f(1,1,1);
-                    // Top left triangle of square
-                    gl.glVertex3d(x, myTerrain.getGridAltitude(x, z), -z-5);
-                    gl.glVertex3d(x, myTerrain.getGridAltitude(x, z+1), -z-5+1);
-                    gl.glVertex3d(x+1, myTerrain.getGridAltitude(x+1, z), -z-5);
-                    gl.glVertex3d(x+1, myTerrain.getGridAltitude(x+1, z+1), -z-5+1);
-                }
-                gl.glEnd();
-                gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
-            }
-        }
+        
+        GLUT glut = new GLUT();
+        GLU glu = new GLU();
+//        glu.gluLookAt(0,-1,0,2.5,0,-2.5,0,1,0);
+        gl.glTranslated(0, -2, -8);
+//        num += 0.8;
+        gl.glRotated(num,0,1,0);
 
-	}
+//        glut.glutSolidTeapot(1);
+        myTerrain.draw(gl);
 
-	@Override
-	public void dispose(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void init(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
-			int height) {
-		// TODO Auto-generated method stub
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        // TODO Auto-generated method stub
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glEnable(GL2.GL_DEPTH_TEST);
+
+        //By enabling lighting, color is worked out differently.
+        gl.glEnable(GL2.GL_LIGHTING);
+
+        //When you enable lighting you must still actually
+        //turn on a light such as this default light.
+        gl.glEnable(GL2.GL_LIGHT0);
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+                        int height) {
+//        // TODO Auto-generated method stub
+//        GL2 gl = drawable.getGL().getGL2();
+//        gl.glMatrixMode(GL2.GL_PROJECTION);
+//        gl.glLoadIdentity();
+//
+//        //You can use an orthographic camera
+//        gl.glOrtho(-2, 2, -2, 2, 0, 20);
+
         GL2 gl = drawable.getGL().getGL2();
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
+
         GLU glu = new GLU();
 
-        glu.gluPerspective(120,1,1,20);
+        glu.gluPerspective(60.0, (float)width/(float)height, 1.0, 20.0);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        switch (e.getKeyCode()) {
+
+            case KeyEvent.VK_UP:
+                num += 5f;
+                break;
+            case KeyEvent.VK_DOWN:
+                num -= 5f;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
     }
 }
