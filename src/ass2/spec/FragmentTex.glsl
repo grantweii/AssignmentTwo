fc5a6275d8bc33ac7041bb9440ab13c129adc794
Glover <Glovebox_@Hotmail.com>
2017-10-22 16:18:40 +1100

    Torch shader lighting

--- src/ass2/spec/FragmentTex.glsl
+++ src/ass2/spec/FragmentTex.glsl
@@ -19,49 +19,55 @@ void main (void) {
     vec3 r = normalize(-reflect(l, n));  
     
     if (nightMode) {
-    
-	  	vec3 lightDir = vec3(gl_LightSource[1].position-ecPos);
-	     
-  		float NdotL = max(dot(n,normalize(lightDir)),0.0);
-		if (NdotL > 0.0) {
-	 
-		 	//if within spotlight
-		    float spotEffect = dot(normalize(gl_LightSource[1].spotDirection), normalize(-lightDir));
-		    if (spotEffect > gl_LightSource[1].spotCosCutoff) {
-		    	//ambient lighting
-			    vec4 l_ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
-			    l_ambient = clamp(l_ambient, 0.0, 1.0);
-			     
-			    //diffuse lighting
-			    vec4 l_diffuse = gl_FrontMaterial.diffuse * max(dot(n,l), 0.0) * gl_LightSource[0].diffuse;
-			    l_diffuse = clamp(l_diffuse, 0.0, 1.0);
-			    
-			    //specular lighting
-			    vec4 l_specular = gl_FrontMaterial.specular 
-			                  * pow(max(dot(r,e), 0.0), 0.3 * gl_FrontMaterial.shininess) * gl_LightSource[0].specular;
-			    l_specular = clamp(l_specular, 0.0, 1.0);
-			
-			    //Final resulting light (ambient + diffuse + specular)
-			    totalLight = (l_ambient + l_diffuse + l_specular);
-			    totalLight = clamp(totalLight, 0.0, 1.0); 
-			} else { 
-				//ambient lighting
-			    vec4 l_ambient = gl_FrontMaterial.ambient * gl_LightSource[1].ambient;
-			    l_ambient = clamp(l_ambient, 0.0, 1.0);
-			     
-			    //diffuse lighting
-			    vec4 l_diffuse = gl_FrontMaterial.diffuse * max(dot(n,l), 0.0) * gl_LightSource[1].diffuse;
-			    l_diffuse = clamp(l_diffuse, 0.0, 1.0);
-			    
-			    //specular lighting
-			    vec4 l_specular = gl_FrontMaterial.specular 
-			                  * pow(max(dot(r,e), 0.0), 0.3 * gl_FrontMaterial.shininess) * gl_LightSource[1].specular;
-			    l_specular = clamp(l_specular, 0.0, 1.0);
-			
-			    //Final resulting light (ambient + diffuse + specular)
-			    totalLight = (l_ambient + l_diffuse + l_specular);
-			    totalLight = clamp(totalLight, 0.0, 1.0); 
-			}
+
+	    vec3 toLight;
+        toLight.x = ecPos.x-lightPosition.x;
+        toLight.y = 0;
+        toLight.z = ecPos.z-lightPosition.z;
+
+        vec3 avRot;
+        avRot.x = cos(avatarRotation*3.141/180);
+        avRot.y = 0;
+        avRot.z = sin(avatarRotation*3.141/180);
+
+        float arg = dot(normalize(toLight),normalize(avRot));
+
+        float angle = acos(arg);
+
+        if (angle < 0.5) {
+            //ambient lighting
+            vec4 l_ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
+            l_ambient = clamp(l_ambient, 0.0, 1.0);
+
+            //diffuse lighting
+            vec4 l_diffuse = gl_FrontMaterial.diffuse * max(dot(n,l), 0.0) * gl_LightSource[0].diffuse;
+            l_diffuse = clamp(l_diffuse, 0.0, 1.0);
+
+            //specular lighting
+            vec4 l_specular = gl_FrontMaterial.specular
+                          * pow(max(dot(r,e), 0.0), 0.3 * gl_FrontMaterial.shininess) * gl_LightSource[0].specular;
+            l_specular = clamp(l_specular, 0.0, 1.0);
+
+            //Final resulting light (ambient + diffuse + specular)
+            totalLight = (l_ambient + l_diffuse + l_specular);
+            totalLight = clamp(totalLight, 0.0, 1.0);
+        } else {
+            //ambient lighting
+            vec4 l_ambient = gl_FrontMaterial.ambient * gl_LightSource[1].ambient;
+            l_ambient = clamp(l_ambient, 0.0, 1.0);
+
+            //diffuse lighting
+            vec4 l_diffuse = gl_FrontMaterial.diffuse * max(dot(n,l), 0.0) * gl_LightSource[1].diffuse;
+            l_diffuse = clamp(l_diffuse, 0.0, 1.0);
+
+            //specular lighting
+            vec4 l_specular = gl_FrontMaterial.specular
+                          * pow(max(dot(r,e), 0.0), 0.3 * gl_FrontMaterial.shininess) * gl_LightSource[1].specular;
+            l_specular = clamp(l_specular, 0.0, 1.0);
+
+            //Final resulting light (ambient + diffuse + specular)
+            totalLight = (l_ambient + l_diffuse + l_specular);
+            totalLight = clamp(totalLight, 0.0, 1.0);
 		}
     } else {
 	    //ambient lighting
